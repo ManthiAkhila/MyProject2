b8d4fe7b64d04b8b4e9691258f4cf8d35c6671f8
ManthiAkhila <akhilamanthi247@gmail.com>
2024-02-15 11:38:47 +0530

    Version2 committed Successfully

---
+++ Version2.2.java
@@ -0,0 +1,7 @@
+class version2.2
+{
+public static void main(String[] args)
+{
+System.out.println("Version2");
+}
+}
---
+++ Version2.3.java
@@ -0,0 +1,7 @@
+class version2.3
+{
+public static void main(String[] args)
+{
+System.out.println("Version2");
+}
+}
